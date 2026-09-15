003fc082527a0d2be23c5d8b453d7125332261c6
martincbidcom <martin.c@bidcom.com.ar>
2026-09-15 16:23:25 -0300

    test: enhance Input component tests for placeholder and button functionality

--- src/app/components/Input/Input.test.tsx
+++ src/app/components/Input/Input.test.tsx
@@ -18,6 +18,17 @@ describe("Input", () => {
     expect(screen.getByPlaceholderText("Enter text")).toBeInTheDocument();
     expect(screen.getByRole("button", { name: "Submit" })).toBeInTheDocument();
   });
+  test("render correctly input and button with text when placeholder is not provided", () => {
+    render(
+      <Input
+        buttonText="Submit"
+        onAction={jest.fn()}
+      />,
+    );
+
+    expect(screen.getByPlaceholderText("Escribe aquí...")).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "Submit" })).toBeInTheDocument();
+  });
   test("call a function when the button is clicked", async () => {
     const handleAction = jest.fn();
     render(
@@ -36,5 +47,40 @@ describe("Input", () => {
 
     expect(handleAction).toHaveBeenCalledTimes(1);
     expect(handleAction).toHaveBeenCalledWith("Test input");
+  });
+
+  test("does not call the function when the input is empty", async () => {
+    const handleAction = jest.fn();
+    render(
+      <Input
+        placeholder="Enter text"
+        buttonText="Submit"
+        onAction={handleAction}
+      />,
+    );
+
+    const button = screen.getByRole("button", { name: "Submit" });
+
+    await user.click(button);
+
+    expect(handleAction).not.toHaveBeenCalled();
+  });
+
+  test("does not call the function when the input is only whitespace", async () => {
+    const handleAction = jest.fn();
+    render(
+      <Input
+        placeholder="Enter text"
+        buttonText="Submit"
+        onAction={handleAction}
+      />,
+    );
+    const button = screen.getByRole("button", { name: "Submit" });
+    const input = screen.getByPlaceholderText("Enter text");
+
+    await user.type(input, '    ');
+    await user.clear(input);
+
+    expect(handleAction).not.toHaveBeenCalled();
   });
 });
